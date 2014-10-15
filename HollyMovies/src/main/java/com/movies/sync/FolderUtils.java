46692bb56db4f901b4b3ff284388e46b53d94979
madmaggie <stan.lidia.roxana@gmail.com>
2014-10-15 09:59:10 +0300

    Update FolderUtils.java

--- HollyMovies/src/main/java/com/movies/sync/FolderUtils.java
+++ HollyMovies/src/main/java/com/movies/sync/FolderUtils.java
@@ -9,6 +9,31 @@ public class FolderUtils {
     }
   }
   
-  public static Set<String> folderList = new HashSet<~>();
-
+  public static Set<String> generateFolderList(String sourcePath) {
+    Set<String> folderList = new HashSet<~>();
+    try (DirectoryStream<Path> movieDir = Files.newDirectoryStream(Paths.get(sourcePath))) {
+      for (Path folder: movieDir) {
+       folderList.add(folder.getFileName().toString));
+      }
+    } catch (IOException e) {
+     e.printStackTrace(); 
+    }
+    return folderList;
+  }
+  
+  public static void printFolderSubset(String message, Set<Movie> folderList) {
+    logger.info(message);
+    for (Movie movie: folderList) {
+      movie.writeMovie();
+    }
+  }
+  
+  public static Set<String> getFolderDifference(String source, String destination) {
+    Set<String> sourceFolder = FolderUtils.generateFolderList(source);
+    Set<String> finalResult = FolderUtils.generateFolderList(source);
+    Set<String> destinationFolder = FolderUtils.generateFolderList(destination);
+    sourceFolder.retainAll(destinationFolder);
+    finalResult.removeAll(sourceFolder);
+    return finalResult;
+  }
 }
