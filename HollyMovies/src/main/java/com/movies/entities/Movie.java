20681f8ce5994ef53f89b5c830f02a5f831a7d91
madmaggie <stan.lidia.roxana@gmail.com>
2014-10-15 10:25:17 +0300

    Update Movie.java

--- HollyMovies/src/main/java/com/movies/entities/Movie.java
+++ HollyMovies/src/main/java/com/movies/entities/Movie.java
@@ -7,19 +7,33 @@ import java.util.HashSet;
  * Created by Rox on 08.09.2014.
  */
 public class Movie {
-    String genre;
+    String path;
     String title;
+    String genre;
     HashSet<Actor> actors;
     String releaseDate;
 
-
-    public Movie (Path moviePath) {
-        title = moviePath.getFileName().toString();
-        genre = moviePath.getParent().getFileName().toString();
+    public Movie (Path completeMoviePath) {
+        title = completeMoviePath.getFileName().toString();
+        genre = completeMoviePath.getParent().getFileName().toString();
+    }
+    
+    public Movie (String myTitle) {
+        title = myTitle;
     }
 
-    public void initMovie() {
-
+    public boolean equals(Movie other) {
+        if (!super.equals(other)) {
+            return false;
+        }
+        if (title.equals(other.title)) {
+            return true;
+        }
+        else return false;
+    }
+    
+    public int hashCode() {
+        return (int) title.CharAt(0);
     }
 
     public void writeMovie() {
